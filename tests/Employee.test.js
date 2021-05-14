2f270414d6b8fa7b954c0fb5aad736b92c4106ec
Mr Sam Law <sam@chongland.com>
2021-05-14 16:35:02 +1000

    Employee Tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -3,13 +3,72 @@ const Employee = require("../lib/Employee");
 describe('Employee', () => {
     // Test for all use cases when initializing a new Employee object
     describe("Initialization", () => {
-        it("should create an object with a name, id an email if provided with valid arguments", () => {
-            const employee = new Employee("Paul", 1, "paul@gmail.com");
+        it("should create an object", () => {
+            const employee = new Employee();
 
             // Verify that the new object has the correct properties
-            expect(employee.name).toEqual("Paul");
-            expect(employee.id).toEqual(1);
-            expect(employee.email).toEqual("paul@gmail.com");
+            expect(typeof employee).toBe("object");
+        });
+        it("should set name via constructor argument", () => {
+            const name = "paul";
+            const employee = new Employee(name);
+
+            // Verify that the new object has the correct properties
+            expect(employee.name).toBe(name);
+        });
+        it("should set id via constructor argument", () => {
+            const id = 1;
+            const employee = new Employee("paul", id);
+
+            // Verify that the new object has the correct properties
+            expect(employee.id).toBe(id);
+        });
+        it("should set email via constructor argument", () => {
+            const email = "paul@gmail.com";
+            const employee = new Employee("paul", 1, email);
+
+            // Verify that the new object has the correct properties
+            expect(employee.email).toBe(email);
+        });
+    });
+
+    describe("getName", () => {
+        it("should get name of employee via getName()", () => {
+            const name = "paul";
+            const employee = new Employee(name);
+
+            // Verify that the new object has the correct properties
+            expect(employee.getName()).toBe(name);
+        });
+    });
+
+    describe("getId", () => {
+        it("should get id of employee via getId()", () => {
+            const id = 1;
+            const employee = new Employee("paul", 1);
+
+            // Verify that the new object has the correct properties
+            expect(employee.getId()).toBe(id);
+        });
+    });
+
+    describe("getEmail", () => {
+        it("should get email of employee via getEmail()", () => {
+            const email = "paul@gmail.com";
+            const employee = new Employee("paul", 1, "paul@gmail.com");
+
+            // Verify that the new object has the correct properties
+            expect(employee.getEmail()).toBe(email);
+        });
+    });
+
+    describe("getRole", () => {
+        it("should get return 'Employee' via getRole()", () => {
+            const role = "Employee";
+            const employee = new Employee("paul", 1, "paul@gmail.com");
+
+            // Verify that the new object has the correct properties
+            expect(employee.getRole()).toBe(role);
         });
     });
 });
